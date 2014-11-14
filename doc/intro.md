2d23e59dea0c272b1b8bb1805c5fcdbd9fce206a
Alexey Orlov <orlov@bos-ltd.ru>
2014-11-14 16:47:06 +0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pcc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pcc/core_test.clj
@@ -0,0 +1,7 @@
+(ns pcc.core-test
+  (:require [clojure.test :refer :all]
+            [pcc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
